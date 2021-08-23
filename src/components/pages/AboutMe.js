d99d998c42fb7205258771f18088af95b3c6af6d
SL <samanthl795@gmail.com>
2021-08-22 20:07:23 -0700

    Add picture in About Me section

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,6 +4,7 @@ function AboutMe() {
   return (
     <div>
       <h2> About Me </h2>
+      <img src={`${process.env.PUBLIC_URL}/sam_lingampalli_photo.jpeg`}  alt="Sam Lingampalli"/>
       <p>
       I am a full stack developer that is open to free lance, contract, and full time work. I have experience with Angular, Javascript, Python, HTML, CSS, MongoDB, node JS, Express, and Ansible. Please click on the projects in the Work section to view some of my projects.
       </p>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Project from "../Project";
+import "./portfolio.css";
 
 function Portfolio() {
   return (
     <div>
       <h2> Portfolio </h2>
+      <div className="project-container">
       <Project name="Pack Your Bags" image="pack_your_bags.jpeg" image_alt="Pack Your Bags" deployed_link="https://glacial-eyrie-53773.herokuapp.com/" github_link="https://github.com/ts795/pack-your-bags" description="This is a website that allows users to create to do lists in preparation for traveling on trips"/>
+      </div>
     </div>
   );
 }
