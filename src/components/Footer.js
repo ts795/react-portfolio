ee56717edc2d20ef66126d28f8a849c63479ea6e
SL <samanthl795@gmail.com>
2021-08-23 17:32:56 -0700

    Use icons for footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import "./footer.css"
 function Footer() {
   return (
     <div id="contact-links" className="content">
-      <a href="https://github.com/ts795" target="_blank" rel="noreferrer">Github</a>
-      <a href="http://www.linkedin.com/in/sam-l-3b3838132" target="_blank" rel="noreferrer">LinkedIn</a>
-      <a href="https://stackoverflow.com/users/15936187/str3ab" target="_blank" rel="noreferrer">Stack Overflow</a>
+      <a href="https://github.com/ts795" target="_blank" rel="noreferrer"><i className="fa fa-github-square fa-5x"></i></a>
+      <a href="http://www.linkedin.com/in/sam-l-3b3838132" target="_blank" rel="noreferrer"><i className="fa fa-linkedin-square fa-5x"></i></a>
+      <a href="https://stackoverflow.com/users/15936187/str3ab" target="_blank" rel="noreferrer"><i className="fa fa-stack-overflow fa-5x"></i></a>
     </div>
   );
 }
